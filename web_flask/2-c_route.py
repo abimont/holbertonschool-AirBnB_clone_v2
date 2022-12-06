259777846bb1417108a01cb8a29ad5f64de24647
abimont <abimontes00@gmail.com>
2022-12-05 20:40:52 -0500

    format added

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,7 +19,7 @@ def hbnb_route():
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     text = text.replace("_", " ")
-    return f"C {text}"
+    return "C {}".format(text)
 
 
 if __name__ == '__main__':
