391b8198fc3df03e377bd7f2d4d49eb1ee9a3a86
abimont <abimontes00@gmail.com>
2022-12-06 14:11:15 -0500

    Improve engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -69,3 +69,7 @@ class DBStorage:
             bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """close public method"""
+        self.__session.remove()
