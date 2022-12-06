d2739e4b83e201e511a0c6b83998fc5b590fb234
abimont <abimontes00@gmail.com>
2022-12-05 20:21:19 -0500

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python3
+""" Flask: Fist steps """
 from flask import Flask
+
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def index():
+def hello_route():
     return "Hello HBNB!"
 
-app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
