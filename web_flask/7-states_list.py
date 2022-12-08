64191ea640f8e6a4bab4541932ec93e6ebfd2b1e
abimont <abimontes00@gmail.com>
2022-12-08 13:23:43 -0500

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,12 +4,11 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def remove_session():
+def teardown(exception):
     """ removes the current SQLAlchemy Session """
     storage.close()
 
@@ -17,7 +16,7 @@ def remove_session():
 @app.route('/states_list', strict_slashes=False)
 def lists_route():
     """ displays a HTML page """
-    objs_ = [obj for obj in storage.all(State).values]
+    objs_ = [obj for obj in storage.all(State).values()]
     return render_template('7-states_list.html', objs_=objs_)
 
 
