46988a0ac92fb0f21973ac50c318e97b8d8fe021
abimont <abimontes00@gmail.com>
2022-12-08 11:38:10 -0500

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Flask: rendering template """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def remove_session():
+    """ removes the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def lists_route():
+    """ displays a HTML page """
+    objs_ = [obj for obj in storage.all(State).values]
+    return render_template('7-states_list.html', objs_=objs_)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
