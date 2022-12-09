cfdccb3daf1fbc89e8a97634b428e5fb695cdfe8
abimont <abimontes00@gmail.com>
2022-12-08 19:58:02 -0500

    States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -27,13 +27,17 @@ def states_route():
 def state_id(id):
     """ displays a HTML page """
     states = [state for state in storage.all(State).values()]
+    state_name = [state.name
+                  for state in storage.all(State).values()
+                  if state.id == id]
     list_ids = [state.id for state in storage.all(State).values()]
     cities = [city for city in list(storage.all(City).values())]
     return render_template(
         '9-states.html',
         cities=cities,
         states=states,
-        id=id, list_ids=list_ids)
+        id=id, list_ids=list_ids,
+        state_name=state_name[0])
 
 
 if __name__ == '__main__':
